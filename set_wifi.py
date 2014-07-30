34f43a08997a2662c83791fdfd91d0dc0bf53b94
Malini Das <mdas@mozilla.com>
2014-07-30 16:29:20 -0400

    update wifi script

--- set_wifi.py
+++ set_wifi.py
@@ -1,8 +1,12 @@
+import sys
+
 from gaiatest import GaiaData
 from marionette import Marionette
+
+assert(len(sys.argv) == 3)
 m = Marionette()
 m.start_session()
-testvars = {"wifi":{"ssid": "${WIFI_NAME}", "keyManagement": "WPA-PSK", "psk": "${WIFI_PWD}"}}
+testvars = {"wifi":{"ssid": sys.argv[1], "keyManagement": "WPA-PSK", "psk": sys.argv[2]}}
 gd = GaiaData(m, testvars=testvars)
 gd.enable_wifi()
 gd.connect_to_wifi()
