33a61de02d822e0c2ff95e26006f28e1cad75bd6
Malini Das <mdas@mozilla.com>
2014-07-31 14:14:15 -0400

    remove executable flag

---
+++ managed_scripts/test.sh
@@ -0,0 +1,26 @@
+TRIES=0
+
+./run.sh &> testoutput&
+
+#while [ $TRIES -lt 15 ]; do
+while [ $TRIES -lt 60 ]; do
+  echo $TRIES
+  #NOTE: this is a rough draft. This makes sure we get to the point where we can
+  # run the semiauto tests, but if there are issues *during* the test, we 
+  # don't catch them. For example, if we can't connect to marionette,
+  # then we still get TEST_END
+  cat testoutput | grep TEST_END
+  if [ $? == 0 ]; then
+    EXIT_CODE=0
+    nc -z localhost 2828
+    if [ $? != 0 ]; then
+      echo "Marionette is not running on the device"
+      EXIT_CODE=1
+    fi
+    kill `ps ux | grep runcertsuite | grep -v grep | awk '{ print $2 }'`
+    exit $EXIT_CODE
+  fi
+  TRIES=$((TRIES+1))
+  sleep 5
+done
+exit 1
